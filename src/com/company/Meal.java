d4315d4cd48fb4a754e510970293cf28b7134dc0
Hungyuuki <hungyuuki777@gmail.com>
2022-02-10 11:37:27 +0700

    done

---
+++ src/com/company/Meal.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public abstract class Meal {
+    public final void doMeal() {
+        prepareIngredients();
+        cook();
+        eat();
+        cleanUp();
+    }
+
+    public abstract void prepareIngredients();
+
+    public abstract void cook();
+
+    public void eat() {
+        System.out.println("Mmm, that's mmmm");
+    }
+
+    public abstract void cleanUp();
+
+}
